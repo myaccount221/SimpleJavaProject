85cde53e600fabe84125f49801d8e749d28d3c06
Sireesha R Palagiri <sireesha.r.palagiri@sap.com>
2019-08-24 20:36:57 +0530

    Second

---
+++ src/com/simple/java/FirstProgram.java
@@ -0,0 +1,14 @@
+package com.simple.java;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		Integer sum = getSum();
+		System.out.println(sum);
+	}
+
+	private static Integer getSum() {
+		return 3 + 2;
+	}
+
+}
